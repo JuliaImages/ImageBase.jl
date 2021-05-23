a6089b1721d5ed71bf9e802618d80d467dd8e31a
Johnny Chen <johnnychen94@hotmail.com>
2021-05-24 06:57:02 +0800

    precompile restrict for Gray/RGB (#2)

--- src/ImageUtils.jl
+++ src/ImageUtils.jl
@@ -9,4 +9,9 @@ using ImageCore.OffsetArrays
 include("restrict.jl")
 include("compat.jl")
 
+if VERSION >= v"1.4.2" # work around https://github.com/JuliaLang/julia/issues/34121
+    include("precompile.jl")
+    _precompile_()
+end
+
 end
