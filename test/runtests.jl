866aa3e3e0125e5f5d95a077e54fd399e79e3faa
Johnny Chen <johnnychen94@hotmail.com>
2021-08-03 22:58:11 +0800

    skip ChainRules ambiguity

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,20 @@ using OffsetArrays: IdentityUnitRange
 
     @testset "Project meta quality checks" begin
         # Not checking compat section for test-only dependencies
-        Aqua.test_all(ImageBase; project_extras=true, deps_compat=true, stale_deps=true, project_toml_formatting=true)
+        ambiguity_exclude_list = [
+            # https://github.com/JuliaDiff/ChainRulesCore.jl/pull/367#issuecomment-869071000
+            Base.:(==),
+        ]
+        Aqua.test_ambiguities([ImageCore, Base, Core], exclude=ambiguity_exclude_list)
+        Aqua.test_all(ImageBase;
+            ambiguities=false,
+            project_extras=true,
+            deps_compat=true,
+            stale_deps=true,
+            project_toml_formatting=true
+        )
         if VERSION >= v"1.2"
-            doctest(ImageBase, manual = false)
+            doctest(ImageBase,manual = false)
         end
     end
     
